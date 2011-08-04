a217f3c1a7bb014774797bd1a9f6e67f8e9358b0
Gregory McQuillan <gmcquillan@gmail.com>
2011-08-04 11:39:51 -0400

    Project class can now create the files and paths given a Skel object

--- model/project.py
+++ model/project.py
@@ -1,7 +1,36 @@
-from model import skel
+import os
+import shutil
+# from model import skel
+from skel import Skel
 
 class Project(object):
     """creates a project given a skeleton and a project name"""
-    def __init__(self, arg):
+    def __init__(self, name = None, skel = None):
         super(Project, self).__init__()
-        self.arg = arg
+
+        self.project_name = name
+        self.skel         = skel
+
+
+    def createFiles(self, dest):
+        """creates all files from the skeleton in the dest dir"""
+        if self.skel:
+            for skelFile in self.skel.filelist:
+                if os.path.exists(skelFile):
+                    destFile = os.path.join(dest, skelFile)
+                    destPath = os.path.abspath(destFile)
+                    destPath = os.path.dirname(destPath)
+                    if not os.path.exists(destPath):
+                        os.makedirs(destPath)
+                    if not os.path.exists(destFile):
+                        shutil.copyfile(skelFile, destFile)
+                    else:
+                        print '[gskel]: File exists, not creating: ' \
+                                + skelFile
+
+if __name__ == '__main__':
+    s = Skel()
+    s.loadFile('f.xml')
+
+    p = Project('test_project', s)
+    p.createFiles('test_output')
