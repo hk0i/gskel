dd89ef209643390b06b91a7d3b5f88cc68ac5c69
Gregory McQuillan <gmcquillan@gmail.com>
2011-08-05 16:31:23 -0400

    added working code to cli.py and created the gskel main script

--- cli/cli.py
+++ cli/cli.py
@@ -4,8 +4,28 @@ from model import gskel
 
 #gskel cli - commandline interface for gskel
 
-parser = argparse.ArgumentParser(prog='gSkel')
-parser.add_argument('-v', help='display version information and quit')
+class CLIParser(object):
+    """docstring for CLIParser"""
+    def __init__(self):
+        super(CLIParser, self).__init__()
 
-args = parser.parse_args()
-print args
+    def parse(self):
+        parser = argparse.ArgumentParser(prog='gSkel')
+        #add version
+        parser.add_argument(
+            '-v',
+            '--version',
+            action='version',
+            version='%(prog)s ' + gskel.VERSION,
+            help='display version information and quit'
+        )
+
+        parser.add_argument('lang', help='language code: c, cpp, java')
+        parser.add_argument(
+            'directive',
+            help='instruction to perform for specified language'
+        )
+
+        args = parser.parse_args()
+        if args:
+            print args
