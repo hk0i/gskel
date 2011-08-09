05aaa0166bef0a3da3a39aa4784013b2ec3c6e3c
Gregory McQuillan <gmcquillan@gmail.com>
2011-08-09 19:19:04 -0400

    Added a simple C++ Class skeleton

---
+++ skel/cpp/class/%CLASSNAME%.cpp
@@ -0,0 +1,1 @@
+#include "%CLASSNAME%.h"
---
+++ skel/cpp/class/%CLASSNAME%.h
@@ -0,0 +1,15 @@
+#ifndef _%CLASSNAME%_H
+#define _%CLASSNAME%_H
+
+class %CLASSNAME%
+{
+    public:
+        %CLASSNAME%(void);
+        virtual ~%CLASSNAME%(void);
+
+    private:
+        /* data */
+};
+
+
+#endif
