4cd6a65b92612fe1f04fb1edafc995db7d703de2
Gregory McQuillan <gmcquillan@gmail.com>
2011-08-11 16:10:31 -0400

    Added Magento PHP as a language and added a sample skeleton for Mage_Core_Block_text Class Files

---
+++ skel/mage/code/block/%BLOCKNAME%.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * %NAMESPACE% %BLOCKNAME% Block
+ *
+ * @author %AUTHOR% <%AUTHOREMAIL%>
+ */
+class %NAMESPACE%_CategoryTopProduct_Block_%BLOCKNAME%
+    extends Mage_Core_Block_Text
+{
+    public function _prepareLayout()
+    {
+        return parent::_prepareLayout();
+    }
+
+    /**
+     * Returns the HTML output for this block
+     *
+     * @return void
+     * @author %AUTHOR%
+     **/
+    protected function _toHtml()
+    {
+        //code...
+    }
+
+}
+
