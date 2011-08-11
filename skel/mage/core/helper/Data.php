a9e1aa272a51859ee72d132a5fbd6d1ac0ae30cc
Gregory McQuillan <gmcquillan@gmail.com>
2011-08-11 16:43:27 -0400

    Added core/helper skeleton and changed name of code/block/text to core/block/text

---
+++ skel/mage/core/helper/Data.php
@@ -0,0 +1,12 @@
+<?php
+
+/**
+ * %NAMESPACE% %MODULENAME% Helper
+ *
+ * @author %AUTHOR% <%AUTHOREMAIL%>
+ */
+class %NAMESPACE%_%MODULENAME%_Helper_Data
+    extends Mage_Core_Helper_Abstract
+{
+
+}
