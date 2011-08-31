750488b89b67b5278cb39a4bb9c6711c183b5235
Gregory McQuillan <gmcquillan@gmail.com>
2011-08-31 09:28:35 -0400

    Added skeleton factory class and YAML Skel class (SkelYaml) from now on creation of skeleton objects should be done through use of skelfactory's create_skel method

---
+++ model/skelfactory.py
@@ -0,0 +1,21 @@
+from logger import log
+from skely import SkelYaml
+from skel import Skel
+
+def create_skel(filename):
+    """returns appropriate Skel-type object based on file name
+
+    examples:
+        filename "skel.yaml" will return a SkelYaml object
+        filename "skel.xml" will return a Skel object
+
+    """
+    log.debug('create_skel: Attempting to create new skeleton')
+    if filename[-4:] == 'yaml':
+        log.debug('create_skel: Creating new yaml skeleton from file: ' + filename)
+        return SkelYaml(filename)
+
+    if filename[-3:] == 'xml':
+        log.debug('create_skel: Creating new xml skeleton from file: ' + filename)
+        return Skel(filename)
+
---
+++ model/skely.py
@@ -0,0 +1,49 @@
+import os
+
+import yaml
+from logger import log
+from skel import Skel
+
+class SkelYaml(Skel):
+    """skel implementation that loads yaml files"""
+    def __init__(self, filename = None):
+        super(SkelYaml, self).__init__()
+        self.filename = filename
+        if filename:
+            self.loadFile(filename)
+
+        del self.infoNodes
+
+        #this holds a yaml skel object loaded from a file using yaml.load()
+        yam = None
+
+    def loadFile(self, filename):
+        """ docstring for loadFile"""
+        if os.path.exists(filename):
+            log.debug('SkelYaml::loadFile: loading file: ' + filename)
+            f = file(filename, 'r')
+            # temporary storage for yaml information
+            skel = yaml.safe_load(f)
+
+            # info ########################################################
+            self.name      = skel['name']
+            self.author    = skel['author']
+            self.directive = skel['directive']
+            self.language  = skel['language']
+
+            self.params  = skel['params']
+            self.filelist  = skel['filelist']
+        else:
+            log.error(
+                'SkelYaml::loadFile: could not load file: "'
+                + filename + '" because it does not exist.'
+            )
+
+    def saveFile(self, filename):
+        """saves skeleton to a yaml document"""
+        print yaml.dump(self)
+
+if __name__ == '__main__':
+    skel = SkelYaml()
+    skel.loadFile('skel/java/class/skel.yaml')
+    skel.saveFile('test')
